b809680726910c8f369894b1cfa8f04d3cb645ad
desahan <157320618+desahan@users.noreply.github.com>
2024-03-27 12:32:38 +0000

    Add files via upload
    added exception coding to catch KeyErrors

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 #     #Access key and value
 #     pass
 #
-import pandas
 # student_data_frame = pandas.DataFrame(student_dict)
 #
 # #Loop through rows of a data frame
@@ -15,13 +14,23 @@ import pandas
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
+import pandas
+
+# TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
 nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dictionary = {row.letter: row.code for (index, row) in nato_df.iterrows()}
 # print(nato_dictionary)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Convert a word to NATO: ").upper()
-word_as_nato = [nato_dictionary[letter] for letter in word]
-print(word_as_nato)
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def make_word():
+    word = input("Convert a word to NATO: ").upper()
+    try:
+            word_as_nato = [nato_dictionary[letter] for letter in word]
+    except KeyError:
+        print("only alphabet letters please")
+        make_word()
+    else:
+        print(word_as_nato)
+
+make_word()

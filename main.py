094cc8c009b3153fb3db58361c06b54eb10004be
desahan <157320618+desahan@users.noreply.github.com>
2024-03-27 12:33:09 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-# #Looping through dictionaries:
-# for (key, value) in student_dict.items():
-#     #Access key and value
-#     pass
-#
-# student_data_frame = pandas.DataFrame(student_dict)
-#
-# #Loop through rows of a data frame
-# for (index, row) in student_data_frame.iterrows():
-#     #Access index and row
-#     #Access row.student or row.score
-#     pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
 
 import pandas
 
